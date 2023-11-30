5933c9ee5e95ec4124708caa7ed5a2b4d62e3721
Maxou974 <maximeabriel974@gmail.com>
2023-11-30 19:25:08 +0100

    adding a bit of swagger

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -2,36 +2,48 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, Res, UseGuard
 import { AuthService } from './auth.service';
 import { Response, Request } from 'express';
 import { AuthDto } from 'src/auth/dto/auth.dto';
-import { Tokens } from 'src/auth/types';
+import { Tokens, TokensDto } from 'src/auth/types/tokens.type';
 import { RtGuard } from 'src/common/guards';
 import { GetCurrentUser, Public } from 'src/common/decorators';
 import { TfaGuard } from 'src/common/guards/tfa.guard';
 import { TfaDto } from './dto/tfa.dto';
+import { ApiBody, ApiCreatedResponse, ApiExcludeEndpoint, ApiOkResponse, ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { FtApiUserDto } from './dto/ftapi.dto';
 
 @Controller('auth')
 export class AuthController {
 
   constructor(private readonly authService: AuthService) {}
 
+	@ApiResponse({
+		status: 200, 
+		description: 'Return url that client needs to be redirected to, to get 42 loging page'})
 	@Public()
 	@Get()
-	redirect(@Res() res: Response): string {
+	redirect(): string {
     	const url = this.authService.getFtAuthenticationUri();
 		return url;
 	}
 
 	@Public()
+	@ApiExcludeEndpoint()
 	@Get('callback')
 	async authentication(@Query() query: AuthDto, @Res() res: Response): Promise<void> {
 		await this.authService.authenticateUser(query, res);
 	}
 
+	@ApiUnauthorizedResponse({
+		description: 'Wrong code'
+	})
+	@ApiOkResponse({
+		description : 'User is logged in and tokens are returned',
+		type: TokensDto
+	})
 	@Public()
 	@UseGuards(TfaGuard)
-	@Post("tfa")
-	async tfa(@Body() body: TfaDto, @GetCurrentUser('sub') userId: number) {
-		console.log(body)
-		return this.authService.verifyTfa(body.code, userId) 
+	@Get("tfa")
+	async tfa(@Query() query: TfaDto, @GetCurrentUser('sub') userId: number) {
+		return this.authService.verifyTfa(query.code, userId) 
 	}
 
 	@Post('logout')
--- back/src/auth/types/tokens.type.ts
+++ back/src/auth/types/tokens.type.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export type Tokens = {
 	access_token: string
+	refresh_token: string
+}
+
+export class TokensDto {
+	@ApiProperty()
+	access_token: string
+	@ApiProperty()
 	refresh_token: string
 }

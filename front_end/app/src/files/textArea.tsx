96f99dcfbcd86c9add7ab05adbe76dd65e519723
tlassara <theo2sennecey@gmail.com>
2023-12-14 15:10:51 +0100

    start chat, can write message and print him

--- front_end/app/src/files/textArea.tsx
+++ front_end/app/src/files/textArea.tsx
@@ -11,7 +11,7 @@ const Message: React.FC<MessageProps> = ({msg, name}) => {
 	return (
 		<div className="message">
 			<Avatar alt={name} src={require('../asset/default.jpg')}/>
-			<div className="message-info">
+			<div className="message-content">
 				<div className='name'>{name}</div>
 				<div className='msg'>{msg}</div>
 			</div>
@@ -32,25 +32,29 @@ const TextArea: React.FC = () => {
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
+			const element = document.getElementById("test");
+			if (element){
+				element.scrollTop = element.scrollHeight;
+				}
 			setMessages([...messages,{ msg:inputValue, name: 'me' }]);
       setInputValue('');
     }
   };
 	return (
-		<div className="textArea">
-			<div className='messages-container'>
+		<div className="textArea" id="test">
+			<div className='messages-container' >
 				{messages.map((message, index) => (
 					<Message key={index} msg={message.msg} name={message.name} />
 				))}
-				<div className="prompt">
-					<Input 
-						placeholder='Message "Friend name"' 
-						style={{width:'100%'}} 
-						value={inputValue}
-					  onChange={(e) => setInputValue(e.target.value)}
-      			onKeyDown={handleKeyDown}
-					/>
-				</div>
+			</div>
+			<div className="prompt">
+				<Input 
+					placeholder='Message "Friend name"' 
+					style={{width:'100%'}} 
+					value={inputValue}
+				  onChange={(e) => setInputValue(e.target.value)}
+      		onKeyDown={handleKeyDown}
+				/>
 			</div>
 		</div>
 	);

f12c81b73a5c0e54136bc2abb6b56e122f3fa0c1
ldutriez-Inspiron <ldutriez@student.42.fr>
2024-01-13 18:46:31 +0100

    🚸 ✨ Add countdown timer for accepted invitations

--- front_end/app/src/components/game/GamePopup/InvitationStatusComponent.tsx
+++ front_end/app/src/components/game/GamePopup/InvitationStatusComponent.tsx
@@ -8,6 +8,8 @@ import CheckIcon from "@mui/icons-material/Check";
 
 import styles from "./GamePopup.module.css";
 
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
 function InvitationStatusComponent({
   gamePopupProps,
   launchMatch,
@@ -15,6 +17,32 @@ function InvitationStatusComponent({
   gamePopupProps: GamePopupProps;
   launchMatch?: () => void;
 }) {
+  const [isAccepted, setIsAccepted] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (gamePopupProps.acceptedInvite === true) {
+      setIsAccepted(true);
+    }
+  }, [gamePopupProps.acceptedInvite]);
+
+  if (isAccepted === true) {
+    return (
+      <CountdownCircleTimer
+        isPlaying
+        duration={5}
+        colors={["#00FF00", "#FF0000"]}
+        colorsTime={[5, 0]}
+        strokeLinecap="square"
+        trailColor="#abc"
+        onComplete={() => {
+          console.log("done");
+        }}
+        size={60}
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    );
+  }
   if (gamePopupProps.sender.id === userId) {
     return <CircularProgress className={styles.CircularProgress} />;
   } else {

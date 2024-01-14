e40d42a27059bd916267d5eeabb46bc84c6cd618
Maxou974 <maximeabriel974@gmail.com>
2024-01-14 18:06:29 +0100

    joining protected channel form css

--- front_end/app/src/pages/channel/channels.tsx
+++ front_end/app/src/pages/channel/channels.tsx
@@ -5,32 +5,40 @@ import '../../css/channel.css';
 import { BACKEND_URL } from '../global/env';
 import axios from 'axios';
 import { Input } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 
 const Channel = ({ channel, popChannel }: any) => {
 
+	const navigate = useNavigate()
+
 	const [password, setPassword] = useState('')
 	const [showPasswordoverlay, setshowPasswordoverlay] = useState(false)
+	const [showPassword, setShowPassword] = useState(false)
+	const [passwordMessage, setPasswordMessage] = useState<string | null>(null)
 
 	const showOverLay = () => {
+		setPasswordMessage(null)
 		setshowPasswordoverlay(true)
 	}
 
-	const hideOverLay = (e?:any) => {
+	const hideOverLay = (e?: any) => {
 		e?.stopPropagation()
+		setPasswordMessage(null)
 		setshowPasswordoverlay(false)
+		setShowPassword(false)
 	}
 
 	const joinPublic = () => {
 		axios.post(BACKEND_URL + '/channel/join/public', { channelId: channel.id }, { withCredentials: true })
-			.then(() => popChannel(channel.id))
+			.then(() => navigate("/channel/" + channel.id))
 			.catch((e) => console.log(e.response.data))
 	}
 
-	const somefunc = () => {
+	const joinChannel = () => {
 		if (channel.mode === "PUBLIC")
 			joinPublic()
-		if(channel.mode === "PROTECTED"){
+		if (channel.mode === "PROTECTED") {
 			showOverLay()
 		}
 	}
@@ -39,37 +47,60 @@ const Channel = ({ channel, popChannel }: any) => {
 		setPassword(e.target.value)
 	}
 
-	const passwordSubmit = (e: any) => {
-		if (e.key === 'Enter') {
-			console.log(password)
-			axios.post(BACKEND_URL + "/channel/join/protected",
-			{channelId: channel.id, password},
-			{withCredentials: true})
-			.then(() => {
-				hideOverLay()
-				popChannel(channel.id)
-			})
-			.catch((e) => console.log(e))
-		}
+	const handleKeyDown = (e: any) => {
+		if (e.key === 'Escape')
+			hideOverLay()
+		if (e.key === 'Enter')
+			passwordSubmit()
+	}
+
+	const passwordSubmit = () => {
+		setPasswordMessage(null)
+		axios.post(BACKEND_URL + "/channel/join/protected",
+			{ channelId: channel.id, password },
+			{ withCredentials: true })
+			.then(() => navigate("/channel/" + channel.id))
+			.catch((e) => setPasswordMessage(e.response.data.message))
 	}
 
 	return (
-		<div className="channel-box" onClick={somefunc}>
-			{showPasswordoverlay && <div className='overlay'>
-					<div className='content'>
-						<div className='QRCode' >
-							<Input placeholder="password..." onChange={handlepasswordChange} onKeyDown={passwordSubmit}/>
-							<button onClick={hideOverLay}>close</button>
-						</div>
-					</div>
-				</div> }
+		<div className="channel-box" onClick={joinChannel}>
+			{showPasswordoverlay && <div className='channel-password-overlay'>
+				<div className='channel-password-wrap' >
+					{passwordMessage ? <div className='password-submit-message'>{passwordMessage}</div> : null}
+					<Input
+						autoFocus={true}
+						className="channel-password-input"
+						placeholder="password..."
+						type={showPassword ? "text" : "password"}
+						onChange={handlepasswordChange}
+						onKeyDown={handleKeyDown} />
+				</div>
+				<button
+					className="channel-password-button"
+					onClick={hideOverLay}>Cancel</button>
+				<button
+					className="channel-password-button"
+					onClick={passwordSubmit}>Submit</button>
+				<div
+					className="channel-show-password-button"
+					onClick={() => setShowPassword(!showPassword)}>{showPassword ? "Hide" : "Show"}</div>
+			</div>}
 			<div className="owner">
-				<img className="ownerimg" src={BACKEND_URL + '/channel/badge/' + channel.id} />
+				<img
+					className="ownerimg"
+					src={BACKEND_URL + '/channel/badge/' + channel.id} />
 			</div>
 			<div className='channelInfo'>
-				<p className='channelName'>{channel.channelName}</p>
-				<p className='channelMode'>{channel.mode}</p>
-				<p className='channelMode'>{channel.members} {channel.members === 1 ? "member" : "members"}</p>
+				<p className='channelName'>
+					{channel.channelName}
+				</p>
+				<p className='channelMode'>
+					{channel.mode}
+				</p>
+				<p className='channelMode'>
+					{channel.members} {channel.members === 1 ? "member" : "members"}
+				</p>
 			</div>
 		</div>
 	);
@@ -85,22 +116,22 @@ const CreateChannel = () => {
 	const [badgeUpload, setbadgeUpload] = useState<null | string>(null)
 	const [channelMessage, setchannelMessage] = useState<null | string>(null)
 
-	const handleNameChange = (e:any) => {
+	const handleNameChange = (e: any) => {
 		setName(e.target.value);
 	};
 
-	const handleModeChange = (e:any) => {
+	const handleModeChange = (e: any) => {
 		setErrorMessage(null)
 		setchannelMessage(null)
 		setbadgeUpload(null)
 		setMode(e.target.value);
 	};
 
-	const handlePasswordChange = (e:any) => {
+	const handlePasswordChange = (e: any) => {
 		setPassword(e.target.value);
 	};
 
-	const handlePasswordConfirmChange = (e:any) => {
+	const handlePasswordConfirmChange = (e: any) => {
 		setPasswordConfirm(e.target.value);
 	};
 
@@ -115,63 +146,63 @@ const CreateChannel = () => {
 		setbadgeUpload(null)
 
 		axios.post(BACKEND_URL + '/channel',
-			{name, password, passwordConfirm, mode},
+			{ name, password, passwordConfirm, mode },
 			{ withCredentials: true })
-		.then((res: any) => {
-			setchannelMessage('channel Created')
-			if (file)
-				axios.postForm(BACKEND_URL + '/channel/upload/badge/' + res.data.channelId, 
-					{'badge': file },
-					{withCredentials: true})
-				.then(() => setbadgeUpload('badge upload success'))
-				.catch((e) => setErrorMessage(e.response.data.message))
-		})
-		.catch((e) => setErrorMessage(e.response.data.message))
+			.then((res: any) => {
+				setchannelMessage('channel Created')
+				if (file)
+					axios.postForm(BACKEND_URL + '/channel/upload/badge/' + res.data.channelId,
+						{ 'badge': file },
+						{ withCredentials: true })
+						.then(() => setbadgeUpload('badge upload success'))
+						.catch((e) => setErrorMessage(e.response.data.message))
+			})
+			.catch((e) => setErrorMessage(e.response.data.message))
 	}
 
 	return (
 		<div className='channel-form'>
-		<form onSubmit={handleSubmit} >
-		<div>
-			<label>
-				Name:
-				<input type="text" value={name} onChange={handleNameChange} />
-			</label>
-			<label>
-				Mode:
-				<select value={mode} onChange={handleModeChange}>
-					<option value="PUBLIC">PUBLIC</option>
-					<option value="PROTECTED">PROTECTED</option>
-					<option value="PRIVATE">PRIVATE</option>
-				</select>
-			</label>
-			{mode === 'PROTECTED' && (
+			<form onSubmit={handleSubmit} >
 				<div>
 					<label>
-						Password:
-						<input type="password" value={password} onChange={handlePasswordChange} />
+						Name:
+						<input type="text" value={name} onChange={handleNameChange} />
 					</label>
-
 					<label>
-						Confirm password:
-						<input
-							type="password"
-							value={passwordConfirm}
-							onChange={handlePasswordConfirmChange}
-						/>
+						Mode:
+						<select value={mode} onChange={handleModeChange}>
+							<option value="PUBLIC">PUBLIC</option>
+							<option value="PROTECTED">PROTECTED</option>
+							<option value="PRIVATE">PRIVATE</option>
+						</select>
 					</label>
+					{mode === 'PROTECTED' && (
+						<div>
+							<label>
+								Password:
+								<input type="password" value={password} onChange={handlePasswordChange} />
+							</label>
+
+							<label>
+								Confirm password:
+								<input
+									type="password"
+									value={passwordConfirm}
+									onChange={handlePasswordConfirmChange}
+								/>
+							</label>
+						</div>
+					)}
+					<label>
+						Badge:
+						<input type="file" onChange={handleFileChange} />
+					</label>
+					<button type="submit" className='button'>Create</button><br></br>
+					{errorMessage && <><span>{errorMessage}</span><br /></>}
+					{channelMessage && <><span>{channelMessage}</span><br /></>}
+					{badgeUpload && <span>{badgeUpload}</span>}
 				</div>
-			)}
-			<label>
-    	      Badge:
-    	      <input type="file" onChange={handleFileChange} />
-    	    </label>
-		   <button type="submit" className='button'>Create</button><br></br>
-		   { errorMessage &&  <><span>{errorMessage}</span><br/></> }
-		   { channelMessage && <><span>{channelMessage}</span><br/></> }
-		   { badgeUpload && <span>{badgeUpload}</span> }
-		</div>
-		</form>
+			</form>
 		</div>
 	);
 };

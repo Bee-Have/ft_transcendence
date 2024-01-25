f844cf6a80b2240900d61254486864b5bea18d58
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2024-01-25 11:42:16 +0100

    ✨ -interactive avatar && interactive username have their popup back

--- front_end/app/src/components/interactive/InteractiveUsername.tsx
+++ front_end/app/src/components/interactive/InteractiveUsername.tsx
@@ -5,7 +5,6 @@ import PopUp from "../popUp";
 import { Friend } from "src/pages/global/friend.dto";
 
 import styles from "./InteractiveUsername.module.css";
-import { useNavigate } from "react-router";
 
 function InteractiveUsername({
   user,
@@ -15,13 +14,11 @@ function InteractiveUsername({
   usage?: "stranger" | "friend" | "invite";
 }) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const navigate = useNavigate()
   return (
     <>
       <h1
         className={styles.InteractiveUsername}
-        onClick={() => navigate(`/profil/${user.id}`)}
-        //onClick={(event) => setAnchorEl(event.currentTarget)}
+        onClick={(event) => setAnchorEl(event.currentTarget)}
       >
         {user.username}
       </h1>
